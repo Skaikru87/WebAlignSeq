4a9885ba83c6eeab09d84c50d217b8ce5cd39107
MKMichalski <mateuszmichalskiK@gmail.com>
2018-08-20 11:59:59 +0200

    initial commit

---
+++ src/main/java/pl/mkm/WebAlignSeq/WebAlignSeqApplication.java
@@ -0,0 +1,12 @@
+package pl.mkm.WebAlignSeq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAlignSeqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAlignSeqApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/mkm/WebAlignSeq/WebAlignSeqApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.mkm.WebAlignSeq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebAlignSeqApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
